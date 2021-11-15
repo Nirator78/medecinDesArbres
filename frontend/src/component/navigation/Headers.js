826c0643e4d92ea1020cddb56d1407d376fcb37b
nolan78 <superpika.nolan@gmail.com>
2021-11-15 16:27:07 +0100

    Modification des couleurs de frontand et ajout du logo

--- frontend/src/component/navigation/Headers.js
+++ frontend/src/component/navigation/Headers.js
@@ -8,7 +8,7 @@ export default function Headers () {
             <span className="sr-only">Workflow</span>
             <img
             className="h-8 w-auto sm:h-10"
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            src="logo.png"
             alt=""
             />
           </a>
@@ -16,11 +16,12 @@ export default function Headers () {
         <div>
           <a
             href="#"
-            className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+            className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-green-400 hover:bg-green-500"
           >
             Sign up
           </a>
-        </div>
+        </div>  
+        <img class="inline object-cover w-12 h-12 mr-2 rounded-full" src="https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image"/>
       </div>
     )
 }
--- frontend/src/component/navigation/NavVertical.js
+++ frontend/src/component/navigation/NavVertical.js
@@ -3,15 +3,15 @@ import { NavLink } from 'react-router-dom';
 
 export default function NavVertical () {
     return (
-        <div className="flex px-4 py-2 rounded-md flex space-x-2 p-4">
+        <div className="flex px-4 py-2 rounded-md flex space-x-2 p-4 text-left">
             <nav>
-                <NavLink exact to='/' className='block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white' activeClassName='text-blue-600 visited:text-purple-600 ...'>
+                <NavLink exact to='/' className='block py-2.5 px-4 rounded transition duration-200 hover:bg-green-400 hover:text-white' activeClassName='text-green-400 visited:text-purple-600 ...'>
                     Accueil
                 </NavLink>
-                <NavLink exact to='/quiz' className='block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white' activeClassName='text-blue-600 visited:text-purple-600 ...'>
+                <NavLink exact to='/quiz' className='block py-2.5 px-4 rounded transition duration-200 hover:bg-green-400 hover:text-white' activeClassName='text-green-400 visited:text-purple-600 ...'>
                     Quiz
                 </NavLink>
-                <NavLink exact to='/visualnovel' className='block py-2.5 px-4 rounded transition duration-200 hover:bg-blue-700 hover:text-white' activeClassName='text-blue-600 visited:text-purple-600 ...'>
+                <NavLink exact to='/visualnovel' className='block py-2.5 px-4 rounded transition duration-200 hover:bg-green-400 hover:text-white' activeClassName='text-green-400 visited:text-purple-600 ...'>
                     Visual Novel
                 </NavLink>
             </nav>
