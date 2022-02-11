43ab505eb62991ffa7035652825a87f670c95f49
Nirator <clementduval786@gmail.com>
2022-02-11 14:58:21 +0100

    debut modif de user

--- frontent-admin/src/app/user/components/modal-user/modal-user.component.ts
+++ frontent-admin/src/app/user/components/modal-user/modal-user.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { NgForm } from '@angular/forms';
 import {UserService} from "../../services/user.service";
 import {ListUserComponent} from "../list-user/list-user.component";
+
 @Component({
   selector: 'app-modal-user',
   templateUrl: './modal-user.component.html',
@@ -11,6 +12,8 @@ import {ListUserComponent} from "../list-user/list-user.component";
 })
 export class ModalUserComponent {
 
+  @Input() mode!: string;
+  @Input() user!: any;
   faPlus = faPlus;
 
   constructor(private modalService: NgbModal, private userService: UserService, private listUserComponent: ListUserComponent) {
@@ -22,7 +25,7 @@ export class ModalUserComponent {
   }
 
   onSubmit(form: NgForm){
-    // Post l'utilisateur 
+    // Post l'utilisateur
     this.userService.createUser(form).then(() => {
       // Ferme le modal
       this.modalService.dismissAll();
