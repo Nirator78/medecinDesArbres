56ac100efd7a229293103a26fb091bb19bbe651a
Nirator <clementduval786@gmail.com>
2022-02-21 19:37:13 +0100

    ajout list parcours ecolo

--- frontent-admin/src/app/parcours-ecolo/components/modal-parcours-ecolo/modal-parcours-ecolo.component.ts
+++ frontent-admin/src/app/parcours-ecolo/components/modal-parcours-ecolo/modal-parcours-ecolo.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {NgForm} from "@angular/forms";
+import {ParcoursEcoloService} from "../../services/parcours-ecolo.service";
+import {ListParcoursEcoloComponent} from "../list-parcours-ecolo/list-parcours-ecolo.component";
 
 @Component({
   selector: 'app-modal-parcours-ecolo',
   templateUrl: './modal-parcours-ecolo.component.html',
   styleUrls: ['./modal-parcours-ecolo.component.css']
 })
-export class ModalParcoursEcoloComponent implements OnInit {
+export class ModalParcoursEcoloComponent {
 
-  constructor() { }
+  @Input() mode!: string;
+  @Input() parcoursecolo: any = {};
+  faPlus = faPlus;
+  file;
 
-  ngOnInit(): void {
+  constructor(private modalService: NgbModal, private parcoursEcoloService: ParcoursEcoloService, private listParcoursEcoloComponent: ListParcoursEcoloComponent) {
+  }
+
+  open(content) {
+    this.modalService.open(content, { ariaLabelledBy: "modal-basic-title", size: "lg"});
+  }
+
+  onSubmit(form: NgForm){
+    let methode = "";
+    if(this.parcoursecolo.id){
+      methode="updateParcoursEcolo"
+      form.value.id = JSON.parse(this.parcoursecolo.id);
+    }
+    else {
+      methode="createParcoursEcolo"
+    }
+
+    // Post du parcours ecolo
+    this.parcoursEcoloService[methode](form).then(async (res) => {
+      // Upload du fichier après création de l'parcoursEcolo
+      if(this.file){
+        const formData = new FormData();
+
+        formData.append("image", this.file);
+
+        await this.parcoursEcoloService.uploadImageParcoursEcolo(res.id, formData);
+      }
+
+      // Ferme le modal
+      this.modalService.dismissAll();
+      // Refresh la liste des utilisateurs
+      this.listParcoursEcoloComponent.refresh();
+    });
+  }
+
+  onFileSelected(event) {
+    this.file = event.target.files[0];
   }
 
 }
--- frontent-admin/src/app/parcours-ecolo/services/parcours-ecolo.service.ts
+++ frontent-admin/src/app/parcours-ecolo/services/parcours-ecolo.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import {ApiService} from "../../services/api.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParcoursEcoloService {
 
-  constructor() { }
+  constructor(private apiService: ApiService) {
+  }
+
+  async getAllParcoursEcolo(): Promise<any>{
+    const response = await this.apiService.getTypeRequest('parcours-ecolos').toPromise() || {};
+    return response["data"];
+  }
+
+  async createParcoursEcolo(user) {
+    const response = await this.apiService.postTypeRequest('parcours-ecolo', user.value).toPromise() || {};
+    return response["data"];
+  }
+
+  async updateParcoursEcolo(user) {
+    const response = await this.apiService.putTypeRequest('parcours-ecolo/' + user.value.id, user.value).toPromise() || {};
+    return response["data"];
+  }
+
+  async deleteParcoursEcolo(id) {
+    return await this.apiService.deleteTypeRequest(`parcours-ecolo/${id}`).toPromise() || {};
+  }
+
+  async uploadImageParcoursEcolo(id, image) {
+    const response = await this.apiService.uploadTypeRequest('parcours-ecolo', id, image).toPromise() || {};
+    return response["data"];
+  }
 }
