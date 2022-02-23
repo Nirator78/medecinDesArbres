bff1d40aa7ec49023cd7e0edfe0c3ca027abf6ae
Nirator <clementduval786@gmail.com>
2022-02-23 19:06:07 +0100

    création module quiz

---
+++ frontent-admin/src/app/statistique/components/quiz-meilleur-joueur/quiz-meilleur-joueur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-meilleur-joueur',
+  templateUrl: './quiz-meilleur-joueur.component.html',
+  styleUrls: ['./quiz-meilleur-joueur.component.css']
+})
+export class QuizMeilleurJoueurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontent-admin/src/app/statistique/components/quiz-ratio-reponse-top/quiz-ratio-reponse-top.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quiz-ratio-reponse-top',
+  templateUrl: './quiz-ratio-reponse-top.component.html',
+  styleUrls: ['./quiz-ratio-reponse-top.component.css']
+})
+export class QuizRatioReponseTopComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
