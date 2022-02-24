3ac6fef8ba5ef5740638f300bd20e55782f04a23
Nirator <clementduval786@gmail.com>
2022-02-24 21:06:08 +0100

    delete onInit dans les stats commande

--- frontent-admin/src/app/statistique/components/commande-panier-moyen/commande-panier-moyen.component.ts
+++ frontent-admin/src/app/statistique/components/commande-panier-moyen/commande-panier-moyen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import {StatistiqueCommandeService} from "../../services/statistique-commande.service";
 
@@ -7,7 +7,7 @@ import {StatistiqueCommandeService} from "../../services/statistique-commande.se
   templateUrl: './commande-panier-moyen.component.html',
   styleUrls: ['./commande-panier-moyen.component.css']
 })
-export class CommandePanierMoyenComponent implements OnInit {
+export class CommandePanierMoyenComponent {
 
   public panierMoyenParMois;
   public environment = environment;
@@ -15,9 +15,6 @@ export class CommandePanierMoyenComponent implements OnInit {
   constructor(private statistiqueCommandeService: StatistiqueCommandeService) {
     this.panierMoyenParMois = [];
     this.refresh();
-  }
-
-  ngOnInit(): void {
   }
 
   async refresh() {
