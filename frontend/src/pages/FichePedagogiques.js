d8f103a27a73fbba006ff86b84611cb61f3bdd85
Nirator78 <clementduval786@gmail.com>
2022-03-23 19:52:55 +0100

    ajout routing dans la liste des fiches

--- frontend/src/pages/FichePedagogiques.js
+++ frontend/src/pages/FichePedagogiques.js
@@ -1,14 +1,33 @@
-import React from 'react';
-import { Paper, Typography } from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import { useNavigate } from "react-router-dom"
+import { Paper, Typography, Grid } from '@mui/material';
+import GreenButton from '../component/GreenButton';
+import FichePedagogiqueService from "../services/fiche-pedagogique.service";
 
 export default function FichePedagogiques(props) {
+    const [fichePedagogiqueList, setFichePedagogiqueList] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(async () => {
+        const response = await FichePedagogiqueService.getAllFichePedagogiques();
+        setFichePedagogiqueList(response);
+    }, [])
+
     return (
         <>
             <Paper>
-                <Typography>
-                    Fiches Pédagogiques
+                <Typography align="center" variant="h4" gutterBottom component="div">
+                    Informez vous grâce à nos supers fiches pédagogiques, adaptées a tous !
                 </Typography>
-                test
+                <Grid container spacing={2}>
+                    {fichePedagogiqueList.map((fiche, index) => {
+                        return (
+                            <Grid item xs={4} key={index}>
+                                <GreenButton title={fiche.titre} handleClick={() => navigate(`/app/fiche-pedagogique/${fiche.id}`)} />
+                            </Grid>
+                        )
+                    })}
+                </Grid>
             </Paper>
         </>
     )
--- frontend/src/pages/subAccueil/FichePedagogiques.js
+++ frontend/src/pages/subAccueil/FichePedagogiques.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from 'react';
+import { useNavigate } from "react-router-dom"
 import { Paper, Typography, Grid } from '@mui/material';
 import GreenButton from '../../component/GreenButton';
 import FichePedagogiqueService from "../../services/fiche-pedagogique.service";
 
 export default function FichePedagogiques(props) {
     const [fichePedagogiqueList, setFichePedagogiqueList] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(async () => {
         const response = await FichePedagogiqueService.getAllFichePedagogiques();
@@ -23,7 +25,7 @@ export default function FichePedagogiques(props) {
                 {fichePedagogiqueList.map((fiche, index) => {
                     return (
                         <Grid item xs={4} key={index}>
-                            <GreenButton title={fiche.titre} handleClick={() => { console.log(fiche.id) }} />
+                            <GreenButton title={fiche.titre} handleClick={() => navigate(`/app/fiche-pedagogique/${fiche.id}`)} />
                         </Grid>
                     )
                 })}
