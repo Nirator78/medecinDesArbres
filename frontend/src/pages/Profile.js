1861be5a1e827ee4d9e394b8ac9360a653fea96a
Nirator78 <clementduval786@gmail.com>
2022-04-03 13:47:26 +0200

    ajout libraire mui lab

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import { Paper, Typography } from '@mui/material';
+import * as React from 'react';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
 import FormProfile from '../component/FormProfile';
+import { Paper, Typography, Box } from '@mui/material';
 
-export default function Profile(props) {
+export default function Profile() {
+    const [value, setValue] = React.useState('1');
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <>
-            <Paper>
-                <Typography>
-                    Profile
+            <Paper align="center">
+                <Typography variant="h4" gutterBottom component="div">
+                    Mon compte
                 </Typography>
-                <FormProfile />
+                <Box sx={{ width: '100%', typography: 'body1',  alignItems: 'flex-start' }}>
+                    <TabContext value={value}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <TabList onChange={handleChange} aria-label="lab API tabs example">
+                                <Tab label="Profile" value="1" />
+                                <Tab label="Mes parcours écolos" value="2" />
+                                <Tab label="Mes commandes" value="3" />
+                            </TabList>
+                        </Box>
+                        <TabPanel value="1">
+                            <FormProfile/>
+                        </TabPanel>
+                        <TabPanel value="2">
+                            Mes parcours écolos
+                        </TabPanel>
+                        <TabPanel value="3">
+                            Mes commandes
+                        </TabPanel>
+                    </TabContext>
+                </Box>
             </Paper>
         </>
-    )
+    );
 }
