bde7c1ee4bad87e6ea66111ab304b2b1da082869
nolan78 <superpika.nolan@gmail.com>
2022-04-04 15:41:59 +0200

    Nav bar visuel

--- frontend/src/component/Headers.js
+++ frontend/src/component/Headers.js
@@ -1,14 +1,15 @@
 import React from "react";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+
 import Toolbar from '@mui/material/Toolbar';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, NavLink } from 'react-router-dom';
 import Connexion from "./Auth/Connexion";
 import Inscription from "./Auth/Inscription";
 import Profile from "./Auth/Profile";
 import AuthService from "../services/auth.service"
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Button, Container } from "@mui/material";
 
 const links = [
     { name: "Accueil", link: "/app" },
@@ -23,39 +24,39 @@ export default function Headers() {
     const user = AuthService.getUser();
 
     return (
-        <Box sx={{ flexGrow: 1, justifyContent: 'space-around' }}>
-            <AppBar position="static" color="inherit">
-                <Toolbar>
-                    <Grid container>
+        <AppBar position="static" style={{ background: '#FFFFFF' }} >
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' } }}>
                         {
                             // important for mapping
                             links.map((link, index) => {
                                 return (
-                                    <Grid item xs={1} key={index}>
-                                        <RouterLink to={link.link}>
-                                            {link.name}
-                                        </RouterLink>
-                                    </Grid>
+                                    <Button style={{color: '#4caf50'}} item key={index}>
+                                        <NavLink className={({ isActive }) => (isActive ? 'activer' : 'inactive')} to={link.link}>
+                                            {link.name} 
+                                        </NavLink>
+                                    </Button>
                                 )
                             })
                         }
-                    </Grid>
-                    {!user && (
-                        <>
-                            <Connexion />
-                            <Inscription />
-                        </>
-                    )}
-                    {user && (
-                        <>
-                            <RouterLink to="/app/panier">
-                                <ShoppingCartIcon />
-                            </RouterLink>
-                            <Profile user={user} />
-                        </>
-                    )}
+                    </Box>
+                        {!user && (
+                            <>
+                                <Connexion />
+                                <Inscription />
+                            </>
+                        )}
+                        {user && (
+                            <>
+                                <RouterLink to="/app/panier">
+                                    <ShoppingCartIcon style={{color: '#4caf50'}} />
+                                </RouterLink>
+                                <Profile user={user} />
+                            </>
+                        )}
                 </Toolbar>
-            </AppBar>
-        </Box>
+            </Container>
+        </AppBar>
     );
 }
