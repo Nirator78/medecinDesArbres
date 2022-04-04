3e4f6e0d078cbc571e83ab69ddd1905e11e7ccbe
nolan78 <superpika.nolan@gmail.com>
2022-04-04 14:12:35 +0200

    Modification nav bar ajout css

--- frontend/src/component/Headers.js
+++ frontend/src/component/Headers.js
@@ -1,16 +1,17 @@
 import React from "react";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
 import Toolbar from '@mui/material/Toolbar';
-import { Link as RouterLink } from 'react-router-dom';
+import {NavLink } from 'react-router-dom';
 import Connexion from "./Auth/Connexion";
 import Inscription from "./Auth/Inscription";
 import Profile from "./Auth/Profile";
 import AuthService from "../services/auth.service"
+import { Button, Container } from "@mui/material";
+import '../index.css';
 
 const links = [
-    { name: "Accueil", link: "/app" },
+    { name: "Accueil", link: "/app"},
     { name: "Fiche Pédagogique", link: "/app/fiche-pedagogique" },
     { name: "Parcours Ecolo", link: "/app/parcours-ecolo" },
     { name: "Quiz", link: "/app/quiz" },
@@ -22,36 +23,36 @@ export default function Headers() {
     const user = AuthService.getUser();
 
     return (
-        <Box sx={{ flexGrow: 1, justifyContent: 'space-around' }}>
-            <AppBar position="static" color="inherit">
-                <Toolbar>
-                    <Grid container>
-                        {
-                            // important for mapping
-                            links.map((link, index) => {
-                                return (
-                                    <Grid item xs={1} key={index}>
-                                        <RouterLink to={link.link}>
-                                            {link.name}
-                                        </RouterLink>
-                                    </Grid>
-                                )
-                            })
-                        }
-                    </Grid>
-                    {!user && (
-                        <>
-                            <Connexion />
-                            <Inscription />
-                        </>
-                    )}
-                    {user && (
-                        <>
-                            <Profile user={user} />
-                        </>
-                    )}
-                </Toolbar>
-            </AppBar>
-        </Box>
+        <AppBar position="static" style={{ background: '#FFFFFF' }} >
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'flex' } }}>
+                {
+                    // important for mapping
+                    links.map((link, index) => {
+                        return (
+                            <Button style={{color: '#4caf50'}} item key={index}>
+                                <NavLink className={({ isActive }) => (isActive ? 'activer' : 'inactive')} to={link.link}>
+                                    {link.name} 
+                                </NavLink>
+                            </Button>
+                        )
+                    })
+                }
+            </Box>
+                {!user && (
+                    <>
+                        <Connexion />
+                        <Inscription />
+                    </>
+                )}
+                {user && (
+                    <>
+                        <Profile user={user} />
+                    </>
+                )}
+          </Toolbar>
+        </Container>
+      </AppBar>
     );
 }
