7b21236923747dcdd92540e4495014946519cae9
nolan78 <superpika.nolan@gmail.com>
2022-04-05 15:39:33 +0200

    Modification du style dans parcours ecolo profile

--- frontend/src/component/Profile/ParcoursEcoloProfileList.js
+++ frontend/src/component/Profile/ParcoursEcoloProfileList.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { Paper, Typography, Grid, Modal, Box, Button } from '@mui/material';
+import { Paper, Typography, Grid, Modal, Box, Button, Card, CardHeader, Avatar, CardMedia, CardContent, CardActions } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ParcoursEcoloService from "../../services/parcours-ecolo.service";
 import AuthService from "../../services/auth.service";
@@ -24,41 +24,65 @@ export default function ParcoursEcoloProfileList() {
 
     return (
         <>
-            <Typography align="center" variant="h4" gutterBottom component="div">
-                Liste des parcours écolos de nos utilisateurs
-            </Typography>
-            <Grid container spacing={2}>
-                {parcoursEcoloList?.map((parcoursEcolo, index) => {
-                    return (
-                        <Grid item xs={4} key={index}>
-                            {
-                                connectedUser.id === parcoursEcolo.user.id ?
-                                    <Button variant="contained" onClick={() => {
-                                        ParcoursEcoloService.deleteParcoursEcolo(parcoursEcolo.id);
-                                        setTimeout(
-                                            () => {
-                                                handleRefresh()
-                                            },
-                                            500
-                                        );
-                                    }}><DeleteIcon></DeleteIcon></Button> :
-                                    null
-                            }
-                            <Typography variant="h6">
-                                {parcoursEcolo.description} {parcoursEcolo.user.nom} {parcoursEcolo.user.prenom}
-                            </Typography>
-                            <img
-                                src={"http://localhost:3000/" + parcoursEcolo?.image?.url}
-                                alt={parcoursEcolo.description}
-                                width="200"
-                            />
-                            <Typography>
-                                Nombre de sac ramassés: {parcoursEcolo.nbSac}
-                            </Typography>
-                        </Grid>
-                    )
-                })}
-            </Grid>
+            <Paper  sx={{ p: 4, mt: 5, ml: 4, mr: 4, mb: 2, borderRadius: 2}}>
+                <Typography align="center" variant="h4" gutterBottom component="div">
+                    Liste des parcours écolos de nos utilisateurs
+                </Typography>
+                <Grid container spacing={{ xs: 2, md: 3, pl: 2}} columns={{ xs: 4, sm: 8, md: 12 }}>
+                    {parcoursEcoloList?.map((parcoursEcolo, index) => {
+                        return (
+                            <Grid item xs={4} key={index}>
+                                <Card sx={{ maxWidth: 345 }}>
+                                    <CardHeader
+                                        avatar={
+                                        <Avatar  aria-label="recipe">
+                                            {parcoursEcolo.user.prenom.substr(0, 1)}{parcoursEcolo.user.nom.substr(0, 1)}
+                                        </Avatar>
+                                        }
+                                        title={parcoursEcolo.user.prenom + " " + parcoursEcolo.user.nom}                                   
+                                        subheader={parcoursEcolo.ville.ville}
+                                    />
+                                    
+                                    <CardMedia
+                                        component="img"
+                                        height="194"
+                                        style={{
+                                            maxWidth: 500,
+                                            maxHeight: 180,
+                                          }}
+                                        image={"http://localhost:3000/" + parcoursEcolo?.image?.url}
+                                        alt={parcoursEcolo.description}
+                                    />
+                                    <CardContent>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {parcoursEcolo.description} 
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            Nombre de sac ramassés: {parcoursEcolo.nbSac}
+                                        </Typography>
+                                        <CardActions style={{justifyContent: 'right'}}>
+                                        {
+                                            connectedUser.id === parcoursEcolo.user.id ?
+                                                <Button variant="contained" onClick={() => {
+                                                    ParcoursEcoloService.deleteParcoursEcolo(parcoursEcolo.id);
+                                                    setTimeout(
+                                                        () => {
+                                                            handleRefresh()
+                                                        },
+                                                        500
+                                                    );
+                                                }}><DeleteIcon></DeleteIcon></Button> :
+                                                null
+                                        }
+                                        </CardActions>
+                                    </CardContent>
+                                    
+                                </Card>
+                            </Grid>
+                        )
+                    })}
+                </Grid>
+            </Paper>
         </>
     )
 }
