aa1f2ffc8e4892f215866908ca5c902d91d85be6
nolan78 <superpika.nolan@gmail.com>
2022-04-05 19:25:04 +0200

    Affichage message si pas de parcours partager

--- frontend/src/component/Profile/ParcoursEcoloProfileList.js
+++ frontend/src/component/Profile/ParcoursEcoloProfileList.js
@@ -3,6 +3,7 @@ import { Paper, Typography, Grid, Modal, Box, Button, Card, CardHeader, Avatar, 
 import DeleteIcon from '@mui/icons-material/Delete';
 import ParcoursEcoloService from "../../services/parcours-ecolo.service";
 import AuthService from "../../services/auth.service";
+import FormParcoursEcolo from '../FormParcoursEcolo';
 
 export default function ParcoursEcoloProfileList() {
     const [parcoursEcoloList, setParcoursEcoloList] = useState([]);
@@ -17,6 +18,7 @@ export default function ParcoursEcoloProfileList() {
         setRefresh(false);
     }, [refresh])
 
+    
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
@@ -26,10 +28,11 @@ export default function ParcoursEcoloProfileList() {
         <>
             <Paper  sx={{ p: 4, mt: 5, ml: 4, mr: 4, mb: 2, borderRadius: 2}}>
                 <Typography align="center" variant="h4" gutterBottom component="div">
-                    Liste des parcours écolos de nos utilisateurs
+                    Vos parcours écolos partager
                 </Typography>
                 <Grid container spacing={{ xs: 2, md: 3, pl: 2}} columns={{ xs: 4, sm: 8, md: 12 }}>
-                    {parcoursEcoloList?.map((parcoursEcolo, index) => {
+                    {parcoursEcoloList?.length ?
+                    parcoursEcoloList?.map((parcoursEcolo, index) => {
                         return (
                             <Grid item xs={4} key={index}>
                                 <Card sx={{ maxWidth: 345 }}>
@@ -80,7 +83,22 @@ export default function ParcoursEcoloProfileList() {
                                 </Card>
                             </Grid>
                         )
-                    })}
+                    }):
+                        <Grid xs={12} pt={4} align="center">
+                            <Typography variant="body2" color="text.secondary" pb={2}> 
+                                Vous n'avez pas de parcours écolos partager avec la communauté
+                            </Typography>
+                            <Button style={{backgroundColor: '#3e993f'}} size="small" variant="contained" sx={{mb: 2}} onClick={handleOpen}>Partager un parcours avec la communauté</Button>
+                            <Modal
+                                open={open}
+                                onClose={handleClose}
+                                aria-labelledby="modal-modal-title"
+                                aria-describedby="modal-modal-description"
+                            >
+                                <FormParcoursEcolo handleClose={handleClose} handleRefresh={handleRefresh}></FormParcoursEcolo>
+                            </Modal>
+                        </Grid>
+                        }
                 </Grid>
             </Paper>
         </>
