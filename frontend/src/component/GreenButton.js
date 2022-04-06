cdf823552124bd5c4ee2d3ae6daa665c251e3143
Nolan78 <superpika.nolan@gmail.com>
2022-04-06 17:15:12 +0200

    Couleur green bouton modifier

--- frontend/src/component/GreenButton.js
+++ frontend/src/component/GreenButton.js
@@ -4,10 +4,11 @@ function GreenButton({ title, handleClick }) {
     return (
         <button
             onClick={handleClick}
-            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" style={{backgroundColor: '#3e993f' }}
         >
             {title}
         </button>
+        
     )
 }
 
