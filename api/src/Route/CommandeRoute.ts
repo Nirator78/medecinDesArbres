1f0a364fac40bfedc6eb906adb0fc74a3e6ad808
Nirator78 <clementduval786@gmail.com>
2022-07-26 11:07:42 +0200

    Ajout sécurité par role dans toute les routes api

--- api/src/Route/CommandeRoute.ts
+++ api/src/Route/CommandeRoute.ts
@@ -3,48 +3,56 @@
  * Created At 09:57
  */
 import { CommandeController } from "../Controller/CommandeController";
+import { UserRole } from "../Entity/User";
 
 export default [
     {
         method: "get",
         route: "/commandes",
         controller: CommandeController,
-        action: "all"
+        action: "all",
+        allowedRoles: [UserRole.SUPERADMIN, UserRole.ADMIN]
     },
     {
         method: "get",
         route: "/commande/:id",
         controller: CommandeController,
-        action: "one"
+        action: "one",
+        allowedRoles: [UserRole.SUPERADMIN, UserRole.ADMIN, UserRole.USER]
     },
     {
         method: "get",
         route: "/user/:id/commandes",
         controller: CommandeController,
-        action: "getCommandeByUser"
+        action: "getCommandeByUser",
+        allowedRoles: [UserRole.SUPERADMIN, UserRole.ADMIN, UserRole.USER]
     },
     {
         method: "post",
         route: "/user/:userId/panier",
         controller: CommandeController,
-        action: "createCommandeByPanier"
+        action: "createCommandeByPanier",
+        allowedRoles: [UserRole.SUPERADMIN, UserRole.ADMIN, UserRole.USER]
     },
     {
         method: "post",
         route: "/commande",
         controller: CommandeController,
-        action: "save"
+        action: "save",
+        allowedRoles: [UserRole.SUPERADMIN, UserRole.ADMIN]
     },
     {
         method: "put",
         route: "/commande/:id",
         controller: CommandeController,
-        action: "update"
+        action: "update",
+        allowedRoles: [UserRole.SUPERADMIN, UserRole.ADMIN]
     },
     {
         method: "delete",
         route: "/commande/:id",
         controller: CommandeController,
-        action: "remove"
+        action: "remove",
+        allowedRoles: [UserRole.SUPERADMIN, UserRole.ADMIN]
     }
 ];
--- api/src/Route/VilleRoute.ts
+++ api/src/Route/VilleRoute.ts
@@ -3,36 +3,42 @@
  * Created At 09:29
  */
 import { VilleController } from "../Controller/VilleController";
+import { UserRole } from "../Entity/User";
 
 export default [
     {
         method: "get",
         route: "/villes",
         controller: VilleController,
-        action: "all"
+        action: "all",
+        allowedRoles: [UserRole.SUPERADMIN, UserRole.ADMIN, UserRole.USER]
     },
     {
         method: "get",
         route: "/ville/:id",
         controller: VilleController,
-        action: "one"
+        action: "one",
+        allowedRoles: [UserRole.SUPERADMIN, UserRole.ADMIN, UserRole.USER]
     },
     {
         method: "post",
         route: "/ville",
         controller: VilleController,
-        action: "save"
+        action: "save",
+        allowedRoles: [UserRole.SUPERADMIN, UserRole.ADMIN]
     },
     {
         method: "put",
         route: "/ville",
         controller: VilleController,
-        action: "update"
+        action: "update",
+        allowedRoles: [UserRole.SUPERADMIN, UserRole.ADMIN]
     },
     {
         method: "delete",
         route: "/ville/:id",
         controller: VilleController,
-        action: "remove"
+        action: "remove",
+        allowedRoles: [UserRole.SUPERADMIN, UserRole.ADMIN]
     }
 ];
