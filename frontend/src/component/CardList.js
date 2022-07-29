f95898128dbcde9edf58d15c829a50f11c4d335c
Nolan78 <superpika.nolan@gmail.com>
2022-07-29 12:01:23 +0200

    Card modifier pour l'affichage du nettoyage

--- frontend/src/component/CardList.js
+++ frontend/src/component/CardList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Box from "@mui/material/Box"
-import { Typography } from "@mui/material";
+import { Avatar, Grid, Typography } from "@mui/material";
 import { useStyles } from "../utils/style.js";
 
 function CardList({ title, data }) {
@@ -8,9 +8,27 @@ function CardList({ title, data }) {
     return (
 
         <Box sx={style.boxBordered}>
-            <Typography variant="h6">
+            <Typography variant="h6" pb={1}>
                 {title}
             </Typography>
+            {
+                data.map((item, index) => {
+                    const userName = item.user.split(" ");
+                    return (
+                        <Grid  key={index} pb={1} container spacing={{ xs: 1, md: 3, pl: 3 }} columns={{ xs: 12, md: 12 }}>
+                            <Grid item xs={1} sx={{ display: { xs: 'none', md: 'flex' } }}>
+                                <Avatar sx={{ width: 32, height: 32 }}>{userName[1].substr(0, 1)}{userName[0].substr(0, 1)}</Avatar>
+                            </Grid>
+                            <Grid item sm={9} xs={8}>
+                                {item.user} 
+                            </Grid>
+                            <Grid item sm={2} xs={4}>
+                                {item.nbSac} sacs
+                            </Grid>
+                        </Grid>
+                    )
+                })
+            }
         </Box>
     )
 }
